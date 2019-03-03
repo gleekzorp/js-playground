55703a34763da24d57bf006a722e49e1048c22fc
gleekzorp <gleek.dan@gmail.com>
2019-03-03 14:34:20 -0700

    Calculate Tip btn displays calculation and reset button.  Reset button resets inputs and display to start.  Merged some styles in stylesheet that had the same attributes.

--- tip-calculator/script-main.js
+++ tip-calculator/script-main.js
@@ -1,3 +1,4 @@
+// Work on making this tipAmmountCalculator less lines
 function tipAmountCalculator() {
     let subTotalValue = Number(document.getElementById('inputSubTotal').value);
     let tipDecimal = Number('.' + document.getElementById('inputTip').value);
@@ -8,26 +9,30 @@ function tipAmountCalculator() {
     let grandTotalValue = tipTotalValue + subTotalValue;
     document.getElementById('grand-total-display').innerHTML = grandTotalValue.toFixed(2);
 
-    changeDisplay()
+    showDisplay()
 }
 
-function changeDisplay() {
-    if (document.getElementById('btn').innerHTML == 'Calculate Tip') {
-        document.getElementById('total-wrapper').style.display='grid';
-        document.getElementById('page-border').style.padding='0 20px 90px 20px';
-        document.getElementById('btn').innerHTML = 'Reset'
-    } else {
-        document.getElementById('btn').innerHTML = 'Calculate Tip'
-        document.getElementById('total-wrapper').style.display='none';
-        document.getElementById('page-border').style.padding='0 20px 20px 20px';
-        resetInputs()
-    }
+function showDisplay() {
+    document.getElementById('calculate-btn-wrapper').style.justifyContent='space-between';
+    document.getElementById('btn-reset').style.display='flex';
+    document.getElementById('total-wrapper').style.display='grid';
+    document.getElementById('page-border').style.padding='0 20px 90px 20px';
+}
+
+function resetDisplay() {
+    document.getElementById('calculate-btn-wrapper').style.justifyContent='center';
+    document.getElementById('total-wrapper').style.display='none';
+    document.getElementById('btn-reset').style.display='none';
+    document.getElementById('page-border').style.padding='0 20px 20px 20px';
+    resetInputs();
 }
 
 function resetInputs() {
     document.getElementById("inputSubTotal").value = '';
     document.getElementById("inputTip").value = '';
 }
-// Helpful Tip.  Put this in the html part of the input field for a different type of reset
+
+// Possible addition for Tip% input field
 // When the input field gets focus, replace its current value with an empty string
 // onfocus="this.value=''"
+// https://www.w3schools.com/howto/howto_html_clear_input.asp
